136a0a9e2d761d817b7e933209995b9d47643787
AzeHie <109618459+AzeHie@users.noreply.github.com>
2023-07-30 10:23:40 +0300

    Exercises 5.7, 5.8 finished

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,45 @@
+import { Fragment, useState } from 'react';
+
+import './Blog.css';
+
 const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
-    <div>
-      {blog.title} {blog.author}
-    </div>
+    <Fragment>
+      {!showDetails && (
+        <div className='blog__title'>
+          {blog.title} by {blog.author}
+          <button
+            onClick={() => {
+              setShowDetails(!showDetails);
+            }}
+          >
+            View
+          </button>
+        </div>
+      )}
+      {showDetails && (
+        <div className='blog__all-details'>
+          <span>
+            Title: {blog.title}
+            <button
+              onClick={() => {
+                setShowDetails(!showDetails);
+              }}
+            >
+              hide
+            </button>
+          </span>
+          <span>Url: {blog.url}</span>
+          <span>
+            Likes: {blog.likes}
+            <button>like</button>
+          </span>
+          <span>Author: {blog.author}</span>
+        </div>
+      )}
+    </Fragment>
   );
 };
 
