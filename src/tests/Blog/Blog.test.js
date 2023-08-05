8c9782bd935899713bf26a2a2ece4b8235438cf4
AzeHie <109618459+AzeHie@users.noreply.github.com>
2023-08-05 14:13:18 +0300

    Exercise 5.14 finished

--- src/tests/Blog/Blog.test.js
+++ src/tests/Blog/Blog.test.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from '../../components/Blog';
 
 describe('<Blog />', () => {
-  test('Component renders blog title', () => {
-    const blog = {
-      title: 'Testing blog component',
-      author: 'Test author',
-      url: 'Some random url',
-      likes: 0,
-      user: {
-        username: 'testing',
-        name: 'test',
-        id: 'testuserid',
-      },
-    };
-
-    const user = {
+  const blog = {
+    title: 'Testing blog component',
+    author: 'Test author',
+    url: 'Some random url',
+    likes: 0,
+    user: {
       username: 'testing',
+      name: 'test',
       id: 'testuserid',
-    };
+    },
+  };
 
+  const user = {
+    username: 'testing',
+    id: 'testuserid',
+  };
+
+  test('Component renders blog title', () => {
     render(<Blog blog={blog} user={user} />);
 
     const element = screen.getByText(/Testing blog component/);
     expect(element).toBeDefined();
+  });
+
+  test('all the blog details are shown, if button clicked', async () => {
+    render(<Blog blog={blog} user={user}/>);
+
+    const userInstance = userEvent.setup();
+    const button = screen.getByText('View');
+    await userInstance.click(button);
+
+    const urlElement = screen.getByText('Url: Some random url');
+    const likesElement = screen.getByText('Likes: 0');
+    const authorElement = screen.getByText('Author: Test author');
+
+    expect(urlElement).toBeDefined();
+    expect(likesElement).toBeDefined();
+    expect(authorElement).toBeDefined();
   });
 });
