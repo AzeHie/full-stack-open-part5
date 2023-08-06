5e111e0c5af7ddc8da7e792d5111ef3d15a20c1a
AzeHie <109618459+AzeHie@users.noreply.github.com>
2023-08-06 10:06:01 +0300

    Some configurations for babel to fix error 'react is not defined' when testing app

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react']
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', { runtime: 'automatic' }],
+  ],
 };
